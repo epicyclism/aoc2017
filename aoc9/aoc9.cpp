f683826c1e011ddb85117b04e638b02494bce27c
Paul <paul@epicyclism.com>
2025-08-09 18:28:25 +0100

    aoc9

--- aoc9/aoc9.cpp
+++ aoc9/aoc9.cpp
@@ -6,31 +6,77 @@
 
 #include <fmt/format.h>
 
-#include "ctre_inc.h"
 #include "timer.h"
 
 auto get_input()
 {
-	return 0;
+	std::string ln;
+	std::getline(std::cin, ln);
+	return ln;
 }
 
-int64_t pt1(auto const& in_addr_t)
+std::string uncancel(std::string const& in)
+{
+	std::string rs;
+	rs.reserve(in.size());
+	for(size_t n = 0; n < in.size(); ++n)
+		if(in[n] == '!')
+			++n;
+		else
+			rs.push_back(in[n]);
+	return rs;
+}
+
+auto ungarbage(std::string const& in)
+{
+	int gc = 0;
+	std::string rs;
+	rs.reserve(in.size());
+	bool in_gbg = false;
+	for(auto c: in)
+	{
+		if(in_gbg)
+		{
+			if( c == '>')
+				in_gbg = false;
+			else
+				++gc;
+		}
+		else
+		{
+			if( c == '<')
+				in_gbg = true;
+			else
+				rs.push_back(c);
+		}
+	}
+	return std::make_pair(rs, gc);
+}
+
+auto pt12(auto const& in)
 {
 	timer t("p1");
-	return 0;
-}
-
-int64_t pt2(auto const& in)
-{
-	timer t("p2");
-	return 0;
+	auto s = uncancel(in);
+	auto[s2, gc] = ungarbage(s);
+	int gn = 1;
+	int scr = 0;
+	for(auto c: s2)
+	{
+		if(c == '{')
+		{
+			scr += gn;
+			++gn;
+		}
+		if(c == '}')
+			--gn;
+	}
+	return std::make_pair(scr, gc);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[ p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
