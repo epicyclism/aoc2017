620e1db6074ad1ceaeeadc2bd661726814365000
Paul <paul@epicyclism.com>
2025-08-10 16:01:34 +0100

    aoc12

--- aoc11/aoc11.cpp
+++ aoc11/aoc11.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
 
 #include <fmt/format.h>
 
--- aoc12/aoc12.cpp
+++ aoc12/aoc12.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <vector>
 
 #include <fmt/format.h>
 
@@ -11,26 +9,63 @@
 
 auto get_input()
 {
-	return 0;
+	std::vector<std::vector<int>> al;
+	std::string ln;
+	while(std::getline(std::cin, ln))
+	{
+		bool one = true;
+		for(auto v : ctre::search_all<"(\\d+)">(ln))
+		{
+			if(one)
+			{
+				one = false;
+				al.emplace_back(std::vector<int>());
+			}
+			else
+				al.back().emplace_back(v.to_number<int>());
+		}
+	}
+	return al;
 }
 
-int64_t pt1(auto const& in_addr_t)
+void count_connected(std::vector<std::vector<int>> const& al, int n, std::vector<bool>& visited, int& cnt)
 {
-	timer t("p1");
-	return 0;
+	for(auto e: al[n])
+	{
+		if(!visited[e])
+		{
+			visited[e] = true;
+			++cnt;
+			count_connected(al, e, visited, cnt);
+		}
+	}
 }
 
-int64_t pt2(auto const& in)
+auto pt12(auto const& in)
 {
-	timer t("p2");
-	return 0;
+	timer t("p12");
+	int p1 = 0;
+	int p2 = 1;
+	std::vector<bool> visited(in.size());
+	count_connected(in, 0, visited, p1);
+	int tmp = 0;
+	int n = 1;
+	while(n != visited.size())
+	{
+		if(!visited[n])
+		{
+			++p2;
+			count_connected(in, n, visited, tmp);
+		}
+		++n;
+	}
+	return std::make_pair(p1, p2);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
