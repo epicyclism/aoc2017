617de85bfe23bbdd9d9bab3785935fba075327c9
Paul <paul@epicyclism.com>
2025-08-10 15:26:34 +0100

    aoc11

--- aoc11/aoc11.cpp
+++ aoc11/aoc11.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <algorithm>
 #include <numeric>
 #include <ranges>
@@ -9,28 +10,88 @@
 #include "ctre_inc.h"
 #include "timer.h"
 
+enum class direct_t { n, ne, se, s, sw, nw, BOGUS};
+direct_t from_v(std::string_view v)
+{
+	if(v == "n")
+		return direct_t::n;
+	if(v == "ne")
+		return direct_t::ne;
+	if(v == "se")
+		return direct_t::se;
+	if(v == "s")
+		return direct_t::s;
+	if(v == "sw")
+		return direct_t::sw;
+	if(v == "nw")
+		return direct_t::nw;
+	return direct_t::BOGUS;
+}
+
 auto get_input()
 {
-	return 0;
+	std::vector<direct_t> r;
+	std::string ln;
+	std::getline(std::cin, ln);
+	for(auto i: ctre::search_all<"([ensw]+)">(ln))
+		r.emplace_back(from_v(i.to_view()));
+	return r;
 }
 
-int64_t pt1(auto const& in_addr_t)
+int steps(int x, int y)
 {
-	timer t("p1");
-	return 0;
+	y = std::abs(y);
+	x = std::abs(x);
+	y -= x;
+	return x + y / 2;
 }
 
-int64_t pt2(auto const& in)
+auto pt12(auto const& in)
 {
-	timer t("p2");
-	return 0;
+	timer t("p12");
+	int mx = 0;
+	int x = 0;
+	int y = 0;
+	for(auto m: in)
+	{
+		switch(m)
+		{
+			case direct_t::n:
+				y += 2;
+				break;
+			case direct_t::ne:
+				++x;
+				++y;
+				break;
+			case direct_t::se:
+				++x;
+				--y;
+				break;
+			case direct_t::s:
+				y -= 2;
+				break;
+			case direct_t::sw:
+				--x;
+				--y;
+				break;
+			case direct_t::nw:
+				--x;
+				++y;
+				break;
+			case direct_t::BOGUS:
+				fmt::println("BOGUS");
+				break;
+		}
+		if(steps(x, y) > mx)
+			mx = steps(x, y);
+	}
+	return std::make_pair(steps(x, y), mx);
 }
 
 int main()
 {
 	auto in = get_input();
-	auto p1 = pt1(in);
-	auto p2 = pt2(in);
+	auto[p1, p2] = pt12(in);
 	fmt::println("pt1 = {}", p1);
 	fmt::println("pt2 = {}", p2);
 }
