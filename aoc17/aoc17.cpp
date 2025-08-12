0f3b9752bc6943fe93990e2b80da36d6d1247d8c
Paul <paul@epicyclism.com>
2025-08-12 01:04:16 +0100

    aoc17

--- aoc17/aoc17.cpp
+++ aoc17/aoc17.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
 #include <string>
-#include <algorithm>
-#include <numeric>
-#include <ranges>
+#include <vector>
 
 #include <fmt/format.h>
 
-#include "ctre_inc.h"
 #include "timer.h"
+#include "ctre_inc.h"
 
-auto get_input()
-{
-	return 0;
-}
-
-int64_t pt1(auto const& in_addr_t)
+int pt1(auto in)
 {
 	timer t("p1");
-	return 0;
+	std::vector<int> v;
+	v.reserve(2048);
+	v.emplace_back(0);
+	int cp = 0;
+	for(int n = 1; n < 2018; ++n)
+	{
+		cp += in;
+		cp %= v.size();
+		v.insert(v.begin() + cp + 1, n);
+		++cp;
+	}
+	return v[cp + 1];
 }
 
-int64_t pt2(auto const& in)
+int pt2(auto in)
 {
 	timer t("p2");
-	return 0;
+	int cp = 0;
+	int n1 = 0;
+	for(int n = 1; n < 50000001; ++n)
+	{
+		cp += in;
+		cp %= n;
+		if(cp == 0)
+			n1 = n;
+		++cp;
+	}
+	return n1;
 }
 
-int main()
+int main(int ac, char** av)
 {
-	auto in = get_input();
+	auto in = 335;
+	if(ac > 1)
+		in = sv_to_t<int>(av[1]);
 	auto p1 = pt1(in);
 	auto p2 = pt2(in);
 	fmt::println("pt1 = {}", p1);
